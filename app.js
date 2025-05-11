4a1d07eb6b06415d77e2dba55160b03522ff32de
yahnzinho1 <yahnzinho1@gmail.com>
2025-05-11 17:11:06 -0300

    AtualizaþÒo do projeto: novas alteraþ§es e arquivos

--- app.js
+++ app.js
@@ -55,8 +55,10 @@ function criarElementoProduto(id, produto) {
   return div;
 }
 
+alert('1');
 // Escuta mudanças no Realtime Database
 onValue(produtosRef, (snapshot) => {
+	alert('2');
   const container = document.getElementById("produtosContainer");
   if (!container) {
     console.error("Elemento com ID 'produtosContainer' não encontrado.");
