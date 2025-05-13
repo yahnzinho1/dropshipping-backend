a6db12b2cb6c457d4b604ec77b56b50783103d16
yahnzinho1 <yahnzinho1@gmail.com>
2025-05-13 11:17:25 -0300

    AtualizaþÒo do projeto: novas alteraþ§es e arquivos

---
+++ public/ComandosPosAlteracoes.bat
@@ -0,0 +1,9 @@
+git add .
+//Esse abaixocomando adiciona todos os arquivos modificados ou novos para serem incluídos no commit.
+git commit -m "Atualização do projeto: novas alterações e arquivos"
+
+//Esse comando abaixo cria um commit com uma mensagem descritiva.
+git pull origin main --rebase
+
+//Esse comando abaixo sincroniza com a versão remota para evitar conflitos antes de enviar.
+git push origin main
