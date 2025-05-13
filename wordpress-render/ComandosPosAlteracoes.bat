aea2e8c74d0d4d60b8b283f9fb776bbfddc0572f
yahnzinho1 <yahnzinho1@gmail.com>
2025-05-13 03:33:02 -0300

    AtualizaþÒo do projeto: novas alteraþ§es e arquivos

--- wordpress-render/ComandosPosAlteracoes.bat
+++
@@ -1,7 +0,0 @@
-cd "C:\Users\User\Desktop\IA-Codes\NesteComputador\PHP & HTML\baseDropshipping\wordpress-render"
-git init
-git remote add origin https://github.com/mauriciowagnerdev/wordpress-render.git
-git branch -M main
-git add .
-git commit -m "Deploy WordPress com Docker"
-git push -u origin main
